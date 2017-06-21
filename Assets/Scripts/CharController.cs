037cf7a2ef8e747ad294526ce78e835a7d81b3d4
Abraham Fowler <abrahamfowler324@gmail.com>
2017-06-21 18:24:14 +0100

    Updating CharController.cs

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -1,5 +1,5 @@
 //REFERENCES: 
-//This source was step the character controller - https://www.youtube.com/watch?v=blO039OzUZc&t=914  
+//This source was for controlling the character - https://www.youtube.com/watch?v=blO039OzUZc&t=914  
 //This source was used to count points - https://www.youtube.com/watch?v=XtR29MmzuT0&list=PLX2vGYjWbI0Q-s4_lX0h4i2zbZqlg4OfF&index=7
 //This source was used to display the score when the game is in play mode - https://www.youtube.com/watch?v=bFSLI2cmYYo&list=PLX2vGYjWbI0Q-s4_lX0h4i2zbZqlg4OfF&index=8
 
